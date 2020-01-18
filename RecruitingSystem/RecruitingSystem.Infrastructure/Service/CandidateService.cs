d24d5d95dfc99f51de0f8bbf08750ce0cac73c72
Marcin K <marcin90k@interia.pl>
2020-01-18 13:21:32 +0100

    Adding Candidate Service

---
+++ RecruitingSystem/RecruitingSystem.Infrastructure/Service/CandidateService.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using RecruitingSystem.Data.Entities;
+using RecruitingSystem.Data.Repositories.Abstract;
+using RecruitingSystem.Infrastructure.Models.Candidate;
+using RecruitingSystem.Infrastructure.Service.Abstract;
+using RecruitingSystem.Infrastructure.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecruitingSystem.Infrastructure.Service
+{
+    public class CandidateService : ICandidateService
+    {
+        private IRepository<Candidate> _repository;
+        private IMapper _mapper;
+
+        public CandidateService(IRepository<Candidate> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public CandidateDTO GetCandidate(Guid id)
+        {
+            var candidate = _repository.GetSingleById(id);
+            var candidateToReturn = _mapper.Map<CandidateDTO>(candidate);
+            return candidateToReturn;
+        }
+
+        public CollectionWithPaginationMetadata<CandidateDTO> GetCandidates(ResourceParameters resourceParameters)
+        {
+            var candidatesAFromRepo = _repository.GetAll();
+
+            if (resourceParameters.SearchQuery != null)
+            {
+                candidatesAFromRepo = ApplySearch(candidatesAFromRepo, resourceParameters.SearchQuery);
+            }
+
+            var pagedCandidates = new PagedList<Candidate>(candidatesAFromRepo, resourceParameters.PageNumber, resourceParameters.PageSize);
+            var candidatesToReturn = _mapper.Map<PagedList<CandidateDTO>>(pagedCandidates);
+
+            return new CollectionWithPaginationMetadata<CandidateDTO>(candidatesToReturn, pagedCandidates.CreateInfo());
+
+        }
+
+        public CandidateDTO AddCandidate(CandidateForManipulationDTO candidate)
+        {
+            var candidateToAdd = _mapper.Map<Candidate>(candidate);
+
+            _repository.Add(candidateToAdd);
+            if (!_repository.Save())
+            {
+                throw new Exception("Adding Candidate has been failed!");
+            }
+
+            return _mapper.Map<CandidateDTO>(candidateToAdd);
+        }
+
+        public void DeleteCandidate(Guid id)
+        {
+            var candidateToDelete = _repository.GetSingleById(id);
+            if (candidateToDelete != null)
+            {
+                _repository.Delete(candidateToDelete);
+                if (!_repository.Save())
+                {
+                    throw new Exception("Candidate deleting has been failed!");
+                }
+            }
+        }
+
+        private IQueryable<Candidate> ApplySearch(IQueryable<Candidate> candidates, string searchQuery)
+        {
+            return candidates.Where(c => c.ExpectedSalary == Int32.Parse(searchQuery)
+                  || c.BasicData.PersonBasicData.LastName == searchQuery
+                  || c.BasicData.PersonBasicData.FirstName == searchQuery
+                  );
+        }
+    }
+}
---
+++ RecruitingSystem/RecruitingSystem.Infrastructure/Service/ExperienceService.cs
@@ -0,0 +1,22 @@
+﻿using RecruitingSystem.Data.Entities;
+using RecruitingSystem.Data.Repositories.Abstract;
+using RecruitingSystem.Infrastructure.Service.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecruitingSystem.Infrastructure.Service
+{
+    public class ExperienceService : IExperienceService
+    {
+        public int GetAmountOfLastJobYears(Guid candidateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAmountOfOveralExperienceYears(Guid candidateId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
