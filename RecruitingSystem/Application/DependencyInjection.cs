078c32a826d77a47908a37ae0c841343c59f4666
Marcin K <marcin90k@interia.pl>
2020-03-26 20:42:01 +0100

    Added DependencyInjection class with extension method to Application.

---
+++ RecruitingSystem/Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using AutoMapper;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
