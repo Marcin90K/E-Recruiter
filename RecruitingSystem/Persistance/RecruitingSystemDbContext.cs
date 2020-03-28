f206e69f73f05047b89cd80f8138c5f625894b4c
Marcin K <marcin90k@interia.pl>
2020-03-28 11:39:00 +0100

    Added DbContext implementation for EF Core in Persistance

---
+++ RecruitingSystem/Persistance/RecruitingSystemDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistance
+{
+    public class RecruitingSystemDbContext : DbContext, IReqruitingSystemDbContext
+    {
+
+        public RecruitingSystemDbContext(DbContextOptions<RecruitingSystemDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<CandidateBasicData> CandidateBasicDatas { get; set; }
+        public DbSet<CandidateJobOffer> CandidateJobOffers { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<JobOffer> JobOffers { get; set; }
+        public DbSet<JobPosition> JobPositions { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<PersonBasicData> PersonBasicDatas { get; set; }
+        public DbSet<Recruiter> Recruiters { get; set; }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+    }
+}
