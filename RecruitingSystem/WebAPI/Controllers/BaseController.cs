97e3c846933d4604d4a6419aa15887c0f18d961e
Marcin K <marcin90k@interia.pl>
2020-04-09 20:35:53 +0200

    Adding base Api Controller

---
+++ RecruitingSystem/WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator =>
+            _mediator != null ? HttpContext.RequestServices.GetService<IMediator>() : null;
+    }
+}
