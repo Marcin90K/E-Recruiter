3c4bc3c3c205ad65fc8d3877df725acd45a56b48
Marcin K <marcin90k@interia.pl>
2020-04-11 11:03:55 +0200

    Added first Controller for JobOffer handling with one endpoint for returning sigle job offers.

---
+++ RecruitingSystem/WebAPI/Controllers/JobOfferController.cs
@@ -0,0 +1,20 @@
+﻿using Application.JobOffers.Queries.GetJobOfferDetail;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebAPI.Controllers
+{
+    public class JobOfferController : BaseController
+    {
+        [HttpGet("joboffer/{id}")]
+        public async Task<IActionResult> GetJobOffer(Guid id)
+        {
+            var jobOffer = await Mediator.Send(new GetJobOfferDetailQuery { Id = id });
+            return Ok(jobOffer);
+        }
+    }
+}
