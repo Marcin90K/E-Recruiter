ce4025a20e64e76c45b6fae6911f81eb4aae02f6
Marcin K <marcin90k@interia.pl>
2020-04-20 21:57:41 +0200

    Added ResourceParameters nd Paging classes

---
+++ RecruitingSystem/Application/Common/Utilities/Pagination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Utilities
+{
+    public class Pagination
+    {
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public int TotalPages 
+        {
+            get
+            {
+                return (int)Math.Ceiling(TotalCount / (double)PageSize); 
+            }
+        }
+        public bool HasPrevious
+        {
+            get { return (CurrentPage > 1); }
+        }
+        public bool HasNext
+        {
+            get { return (CurrentPage < TotalPages); }
+        }
+
+        public Pagination(int currentPage, int pageSize, int totalCount)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+        }
+    }
+}
---
+++ RecruitingSystem/Application/JobOffers/Queries/GetJobOfferList/JobOfferListVm.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Models.JobOffer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.JobOffers.Queries.GetJobOfferList
+{
+    public class JobOfferListVm
+    {
+        public IList<JobOfferDTO> JobOffers { get; set; }
+        public int Count { get; set; }
+    }
+}
