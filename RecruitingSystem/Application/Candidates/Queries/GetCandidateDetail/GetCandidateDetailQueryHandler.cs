7f8d410d81a1c0878da047bc431fb0de48204a0a
Marcin K <marcin90k@interia.pl>
2020-05-01 13:05:07 +0200

    Added Getting single Candidate resouerce action

---
+++ RecruitingSystem/Application/Candidates/Queries/GetCandidateDetail/GetCandidateDetailQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models.Candidate;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Candidates.Queries.GetCandidateDetail
+{
+    public class GetCandidateDetailQueryHandler : IRequestHandler<GetCandidateDetailQuery, CandidateDTO>
+    {
+        private readonly IReqruitingSystemDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetCandidateDetailQueryHandler(IReqruitingSystemDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CandidateDTO> Handle(GetCandidateDetailQuery request, CancellationToken cancellationToken)
+        {
+            var candidateEntity = await _context.Candidates.Where(c => c.Id == request.Id)
+                .Include(c => c.BasicData).ThenInclude(d => d.PersonBasicData)
+                .Include(c => c.BasicData).ThenInclude(d => d.Address)
+                .Include(c => c.CandidateJobOffers)
+                .Include(c => c.Educations)
+                .Include(c => c.Experiences)
+                .FirstOrDefaultAsync();
+
+            if (candidateEntity == null)
+            {
+                throw new ResourceNotFoundException($"Not found candidate with id: {request.Id}");
+            }
+
+            return _mapper.Map<CandidateDTO>(candidateEntity);
+        }
+    }
+}
---
+++ RecruitingSystem/WebAPI/Controllers/CandidateController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Candidates.Queries.GetCandidateDetail;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/candidate")]
+    public class CandidateController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CandidateController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCandidate(Guid id)
+        {
+            var candidate = await _mediator.Send(new GetCandidateDetailQuery() { Id = id });
+            return Ok(candidate);
+        }
+    }
+}
