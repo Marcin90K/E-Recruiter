48ae3cbb8241b3b8eec53f9190d55042b5ce1165
Marcin K <marcin90k@interia.pl>
2020-05-30 17:17:38 +0200

    Created login page

--- RecruitingSystem/RecruitingSystem.Client/RecrutingForm/src/app/auth/auth.module.ts
+++ RecruitingSystem/RecruitingSystem.Client/RecrutingForm/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { AuthComponent } from './auth.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { AuthComponent } from './auth.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ]
 })
--- RecruitingSystem/RecruitingSystem.Client/RecrutingForm/src/app/auth/login/login.component.ts
+++ RecruitingSystem/RecruitingSystem.Client/RecrutingForm/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { LoginBasic } from 'src/app/shared/models/auth/login-basic';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  model: LoginBasic;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.createFormGroup(this.formBuilder);
+  }
 
   ngOnInit() {
+  }
+
+  createFormGroup(fb: FormBuilder) {
+    return fb.group({
+      username: [''],
+      password: ['']
+    });
+  }
+
+  submit() {
+    this.model = this.loginForm.value;
+    console.log(this.model);
   }
 
 }
