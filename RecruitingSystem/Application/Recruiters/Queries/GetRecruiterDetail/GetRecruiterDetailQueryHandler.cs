9d3fd49f5f5f8295400039157b4e2e33d335a383
Marcin K <marcin90k@interia.pl>
2020-09-25 23:57:53 +0200

    Added Recruiter queries on backed side

---
+++ RecruitingSystem/Application/Recruiters/Queries/GetRecruiterDetail/GetRecruiterDetailQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models.Recruiter;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Recruiters.Queries.GetRecruiterDetail
+{
+    public class GetRecruiterDetailQueryHandler : IRequestHandler<GetRecruiterDetailQuery, RecruiterDTO>
+    {
+        private readonly IReqruitingSystemDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetRecruiterDetailQueryHandler(IReqruitingSystemDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<RecruiterDTO> Handle(GetRecruiterDetailQuery request, CancellationToken cancellationToken)
+        {
+            var entity = _context.Recruiters.Where(r => r.Id == request.Id)
+                .Include(r => r.Manager)
+                .Include(r => r.OwnedJobOffers)
+                .FirstOrDefault();
+
+            if (entity == null)
+            {
+                throw new ResourceNotFoundException($"Not found Recruiter with id: {request.Id}");
+            }
+
+            return _mapper.Map<RecruiterDTO>(entity);
+        }
+    }
+}
---
+++ RecruitingSystem/Application/Recruiters/Queries/GetRecruiterList/GetRecruiterListQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Utilities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Recruiters.Queries.GetRecruiterList
+{
+    public class GetRecruiterListQuery : IRequest<RecruiterListVm>
+    {
+        public ResourceParameters ResourceParameters { get; set; }
+    }
+}
