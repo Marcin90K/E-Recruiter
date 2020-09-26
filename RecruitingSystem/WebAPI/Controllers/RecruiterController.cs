1bf919ff97c3fa0900826d940b3da56ae2de2422
Marcin K <marcin90k@interia.pl>
2020-09-27 00:28:23 +0200

    Created methods for getting Recruitermresources on the backend side

---
+++ RecruitingSystem/WebAPI/Controllers/RecruiterController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Application.Recruiters.Queries.GetRecruiterDetail;
+using Application.Common.Utilities;
+using Application.Recruiters.Queries.GetRecruiterList;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/recruiter")]
+    public class RecruiterController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RecruiterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRecruiter(Guid id)
+        {
+            var recruiter = await _mediator.Send(new GetRecruiterDetailQuery() { Id = id });
+            return Ok(recruiter);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRecruiters([FromQuery] ResourceParameters resourceParameters)
+        {
+            var query = new GetRecruiterListQuery() { ResourceParameters = resourceParameters };
+            var recruiters = await _mediator.Send(query);
+            return Ok(recruiters);
+        }
+    }
+}
