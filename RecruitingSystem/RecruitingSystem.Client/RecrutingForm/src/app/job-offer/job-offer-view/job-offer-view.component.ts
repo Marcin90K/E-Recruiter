337be258b6ac9b8ea2b3e94b6987f97a762cd8eb
Marcin K <marcin90k@interia.pl>
2020-10-13 12:15:19 +0200

    Customized job offer view form

--- RecruitingSystem/RecruitingSystem.Client/RecrutingForm/src/app/job-offer/job-offer-view/job-offer-view.component.ts
+++ RecruitingSystem/RecruitingSystem.Client/RecrutingForm/src/app/job-offer/job-offer-view/job-offer-view.component.ts
@@ -2,6 +2,7 @@ import { JobOfferService } from './../../shared/services/job-offer.service';
 import { Component, OnInit } from '@angular/core';
 import { JobOfferVM } from 'src/app/shared/models/job-offer/job-offer-vm';
 import { ActivatedRoute } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-job-offer-view',
@@ -10,17 +11,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class JobOfferViewComponent implements OnInit {
 
-  private viewModel: JobOfferVM;
+  private viewModel: JobOfferVM = {
+    id: '',
+    jobPosition: {
+      id: '',
+      name: ''
+    },
+    description: '',
+    dateOfAdding: null,
+    dateOfExpiration: null,
+    requirements: '',
+    owner: null
+  };
+
   private jobOfferId: string;
+  private dateOfExpiration: string;
+  private dateFormat = 'yyyy-MM-dd';
 
   constructor(private jobOfferService: JobOfferService,
-              private activatedRoute: ActivatedRoute ) {
+              private activatedRoute: ActivatedRoute,
+              private datePipe: DatePipe ) {
     this.jobOfferId = this.activatedRoute.snapshot.parent.params['id'];
     console.log(this.jobOfferId);
 
     this.jobOfferService.getJobOffer(this.jobOfferId).subscribe(
       result => {
         this.viewModel = result;
+        this.dateOfExpiration = this.parseDate(this.viewModel.dateOfExpiration);
         console.log(this.viewModel);
       },
       error => console.log(error)
@@ -28,6 +45,15 @@ export class JobOfferViewComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+
+  apply() {
+
+  }
+
+  parseDate(date: Date) {
+    return this.datePipe.transform(date, this.dateFormat);
   }
 
 }
